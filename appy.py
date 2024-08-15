fd15d6b8160891af3a24687af4489ed2d88e0f0d
ajit <khandaleajit@gmail.com>
2024-08-15 23:59:46 +0530

    fixed linting issues

--- appy.py
+++ appy.py
@@ -1,12 +1,11 @@
-def add(a,b):
-    return a+b
+def add(a, b):
+    return a + b
 
 
+def sub(a, b):
+    return a - b
 
-def sub(a,b):return a-b
+result1 = add(3, 5)
+result2 = sub(3, 1)
 
-result1=add(3,5)
-result2=sub(3,1)
-
-
-print(result1,result2)
+print(result1, result2)
